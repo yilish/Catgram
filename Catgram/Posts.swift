b1b43ec342eb121d0a3741ca31089f367310d453
Zhichen Ren <1337795211@qq.com>
2021-06-18 21:54:21 +0800

    Update resources

--- Catgram/Posts.swift
+++ Catgram/Posts.swift
@@ -19,9 +19,9 @@ struct Post {
 extension Post {
     static var testData = [
         Post(date: "1小时前", content: "可爱🐱🐱～", mood: "@Happy", image: UIImage(named: "cat1-happy")!, isLike: false),
-        Post(date: "1小时前", content: "可爱🐱🐱～", mood: "@Happy", image: UIImage(named: "cat1-happy")!, isLike: false),
-        Post(date: "1小时前", content: "可爱🐱🐱～", mood: "@Happy", image: UIImage(named: "cat1-happy")!, isLike: false),
-        Post(date: "1小时前", content: "可爱🐱🐱～", mood: "@Happy", image: UIImage(named: "cat1-happy")!, isLike: false),
-        Post(date: "1小时前", content: "可爱🐱🐱～", mood: "@Happy", image: UIImage(named: "cat1-happy")!, isLike: false)
+        Post(date: "1小时前", content: "小可怜🐱来抱抱", mood: "@Sad", image: UIImage(named: "cat2-sad")!, isLike: false),
+        Post(date: "1小时前", content: "小🐱🐱趴趴", mood: "@Sad", image: UIImage(named: "cat3-sad")!, isLike: false),
+        Post(date: "1小时前", content: "🐱🐱🌸🌸！", mood: "@Happy", image: UIImage(named: "cat4-happy")!, isLike: false),
+        Post(date: "1小时前", content: "呜呜呜好可怜🥺", mood: "@Sad", image: UIImage(named: "cat7-sad")!, isLike: false)
     ]
 }
